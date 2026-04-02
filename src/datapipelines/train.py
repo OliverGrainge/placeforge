5bf7312e469e96ee04a681242296ef6749a45180
oliver grainge <oliver@grainge.me>
2026-04-02 15:08:14 +0100

    updating code:

--- src/datapipelines/train.py
+++ src/datapipelines/train.py
@@ -110,6 +110,115 @@ def build_sf_xl_small_curavpr_con() -> Pipeline:
 
 
 
+@register_pipeline("sf_xl_small_cosin_00", category="train")
+def build_sf_xl_small_curavpr_con() -> Pipeline:
+    name = "sf_xl_small_cosin_00"
+    return Pipeline(
+        name,
+        steps=[
+            ReadTrainImagesStep(data_root=raw_dir() / SF_XL_SMALL_PATH / "small" / "train"),
+            ComputeImageEmbeddingStep(
+                image_embedding_name="sf_xl_small", batch_size=128, num_workers=8
+            ),
+            AssignCuraVPRPlaceIdStep(
+                image_embedding_name="sf_xl_small",
+                cell_size_meters=10.0,
+                heading_size_degrees=30.0,
+                cos_sim_threshold=0.00,
+                min_images=4,
+            ),
+            AggregatePlaceEmbeddingStep(
+                image_embedding_name="sf_xl_small",
+                place_embedding_name=name,
+                reduction="mean",
+                normalize=True,
+            ),
+            AssignCuraVPRSuperGroupStep(
+                place_embedding_name=name,
+                supergroup_size=512,
+                kmeans_max_iter=100,
+                seed=42,
+            ),
+            SaveTrainDataset(name=name),
+            SummaryTrainDataset(name=name),
+            AnalyseTrainDatasetStep(name=name),
+        ],
+    )
+
+
+
+@register_pipeline("sf_xl_small_cosin_15", category="train")
+def build_sf_xl_small_curavpr_con() -> Pipeline:
+    name = "sf_xl_small_cosin_00"
+    return Pipeline(
+        name,
+        steps=[
+            ReadTrainImagesStep(data_root=raw_dir() / SF_XL_SMALL_PATH / "small" / "train"),
+            ComputeImageEmbeddingStep(
+                image_embedding_name="sf_xl_small", batch_size=128, num_workers=8
+            ),
+            AssignCuraVPRPlaceIdStep(
+                image_embedding_name="sf_xl_small",
+                cell_size_meters=10.0,
+                heading_size_degrees=30.0,
+                cos_sim_threshold=0.15,
+                min_images=4,
+            ),
+            AggregatePlaceEmbeddingStep(
+                image_embedding_name="sf_xl_small",
+                place_embedding_name=name,
+                reduction="mean",
+                normalize=True,
+            ),
+            AssignCuraVPRSuperGroupStep(
+                place_embedding_name=name,
+                supergroup_size=512,
+                kmeans_max_iter=100,
+                seed=42,
+            ),
+            SaveTrainDataset(name=name),
+            SummaryTrainDataset(name=name),
+            AnalyseTrainDatasetStep(name=name),
+        ],
+    )
+
+
+
+@register_pipeline("sf_xl_small_cosin_03", category="train")
+def build_sf_xl_small_curavpr_con() -> Pipeline:
+    name = "sf_xl_small_cosin_00"
+    return Pipeline(
+        name,
+        steps=[
+            ReadTrainImagesStep(data_root=raw_dir() / SF_XL_SMALL_PATH / "small" / "train"),
+            ComputeImageEmbeddingStep(
+                image_embedding_name="sf_xl_small", batch_size=128, num_workers=8
+            ),
+            AssignCuraVPRPlaceIdStep(
+                image_embedding_name="sf_xl_small",
+                cell_size_meters=10.0,
+                heading_size_degrees=30.0,
+                cos_sim_threshold=0.3,
+                min_images=4,
+            ),
+            AggregatePlaceEmbeddingStep(
+                image_embedding_name="sf_xl_small",
+                place_embedding_name=name,
+                reduction="mean",
+                normalize=True,
+            ),
+            AssignCuraVPRSuperGroupStep(
+                place_embedding_name=name,
+                supergroup_size=512,
+                kmeans_max_iter=100,
+                seed=42,
+            ),
+            SaveTrainDataset(name=name),
+            SummaryTrainDataset(name=name),
+            AnalyseTrainDatasetStep(name=name),
+        ],
+    )
+
 
 
 @register_pipeline("gsvcities_curavpr", category="train")
@@ -137,55 +246,17 @@ def build_sf_xl_small_gsvcities_curavpr() -> Pipeline:
             ),
             AssignCuraVPRSuperGroupStep(
                 place_embedding_name=name,
-                supergroup_size=512, #1024,
+                supergroup_size=512,
                 kmeans_max_iter=100,
                 seed=42,
+                N=1,    # default is 5
+                L=1,    # default is 2
             ),
             SaveTrainDataset(name=name),
             SummaryTrainDataset(name=name),
             AnalyseTrainDatasetStep(name=name),
         ],
     )
-
-
-@register_pipeline("sf_xl_small_gsvcities_curavpr", category="train")
-def build_sf_xl_small_gsvcities_curavpr() -> Pipeline:
-    name = "sf_xl_small_gsvcities_curavpr"
-    return Pipeline(
-        name,
-        steps=[
-            ReadTrainImagesStep(data_root=raw_dir() / SF_XL_SMALL_PATH / "small" / "train" / "37.70", source="sf_xl_small"),
-            ReadGSVCitiesTrainImagesStep(data_root=raw_dir() / GSVCITIES_PATH, source="gsvcities", cities=["PRG"]),
-            ComputeImageEmbeddingStep(
-                image_embedding_name="sf_xl_small_gsvcities", batch_size=16, num_workers=8
-            ),
-            AssignCuraVPRPlaceIdStep(
-                image_embedding_name="sf_xl_small_gsvcities",
-                cell_size_meters=10.0,
-                heading_size_degrees=30.0,
-                cos_sim_threshold=0.3,
-                min_images=4,
-            ),
-            AggregatePlaceEmbeddingStep(
-                image_embedding_name="sf_xl_small_gsvcities",
-                place_embedding_name=name,
-                reduction="mean",
-                normalize=True,
-            ),
-            AssignCuraVPRSuperGroupStep(
-                place_embedding_name=name,
-                supergroup_size=512, #1024,
-                kmeans_max_iter=100,
-                seed=42,
-            ),
-            SaveTrainDataset(name=name),
-            SummaryTrainDataset(name=name),
-            AnalyseTrainDatasetStep(name=name),
-        ],
-    )
-
-
-
 
 
 
